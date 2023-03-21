cb582aa4d714b6a897275d81bb2ac9a1ff1b8c22
Bryan Yang <41586021+BryanDYang@users.noreply.github.com>
2023-03-21 16:22:10 -0700

    initial js setup

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -5,3 +5,21 @@ let computerScore = 0;
 let playerScore = 0;
 
 let button = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+const endAlrt = document.querySelector("#end-alert");
+const endDesc = document.querySelector("#end-desc");
+const returnMainBtn = document.querySelector("retry-btn");
+const desc = document.querySelector("#desc3");
+const container = document.querySelector("#results-container");
+
+body.addEventListener("click", skipAnime());
+body.addEventListener("keydown", skipAnime());
+
+function skipAnime () {
+    const span = document.querySelectorAll("span");
+    
+    span.forEach((span) => span.classList.add("skip"));
+}
+
+
