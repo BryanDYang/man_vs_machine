dd9abfa6e860b6e8778353219e91b442949b6026
Bryan Yang <41586021+BryanDYang@users.noreply.github.com>
2023-03-22 06:49:28 -0700

    js update playround function

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -108,11 +108,92 @@ function fadeIn () {
     }
 }
 
-button.forEach(button) => {
+button.forEach((button) => {
     button.addEventListener("click", () => {
         const img = button.querySelector("img");
         playerSelection = img.alt.toLowerCase();
 
-        
-    })
+        playRound(playerSelection, computerSelection);
+
+        if (playerSelection === 5 || computerSelection === 5) {
+            declareWinner();
+        }
+    });
+});
+
+const myArray = ["Rock", "Paper", "Scissors"];
+
+function computerPlay() {
+    return myArray[~~(Math.random() * myArray.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay().toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
+    if (computerSelection == playerSelection) {
+        displayResults("Tie Game!");
+    } else if (
+        (computerSelection == "rock" && playerSelection == "scissors") ||
+        (computerSelection == "scissors" && playerSelection == "paper") ||
+        (computerSelection == "paper" && playerSelection == "rock")
+    ) {
+        computerScore = ++computerScore;
+        keepCpuScore();
+        if (computerScore === 1) {
+            displayResults (
+                `Oh no! You lost.
+                ${capitalize(computerSelection)} beats ${playerSelection}.`
+            );
+        } else if (computerScore === 2) {
+            displayResults(
+                `Arghh. ${capitalize(
+                    computerSelection
+                )} beats ${playerSelection}. Give it another shot!`
+            );
+        } else if (computerScore === 3) {
+            displayResults (
+                `${capitalize(
+                    computerSelection
+                )} beats ${playerSelection}. It's ok. You got this!!`
+            );
+        } else if (computerScore === 4) {
+            displayResults(
+                `Oh no. It's match point!! ${capitalize(
+                    computerSelection
+                )} beats ${playerSelection}. Don't let us down!`
+            );
+        } else {
+            displayResults(`${computerSelection} beats ${playerSelection}`);
+        }
+    } else {
+        playerScore = ++playerScore;
+        keepPlayerScore();
+        if (playerScore == 1) {
+            displayResults(
+                `Let's go! You won!!
+                ${capitalize(playerSelection)} beats ${computerSelection}.`
+            );
+        } else if (playerScore === 2) {
+            displayResults(
+                `You're pretty good at this. ${capitalize(
+                    playerSelection
+                )} beats ${computerSelection}.`
+            );
+        } else if (playerScore === 3){
+            displayResults(
+                `${capitalize(
+                    playerSelection
+                )} beats ${computerSelection}! Has mankind found its savior??`
+            );
+        } else if (playerScore === 4){
+            displayResults(
+                `${capitalize(
+                    playerSelection
+                )} beats ${computerSelection}. One more and you're a hero!`
+            );
+        } else {
+            displayResults(`${playerSelection} beats ${computerSelection}`);
+        }
+    }
+}
+
